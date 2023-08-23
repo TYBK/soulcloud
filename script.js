a73eb3e88689e9bff1e76d93962f8537ef91e16e
TYBK <135532606+TYBK@users.noreply.github.com>
2023-08-23 08:18:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// parallax animation //
+
+window.addEventListener('scroll', () => {
+    const parallaxBg = document.querySelectorAll('.parallax-bg');
+    parallaxBg.forEach(element => {
+      const scrollPosition = window.pageYOffset;
+      element.style.transform = `translateY(${scrollPosition * 0.5}px)`; // Adjust the multiplier for desired effect
+    });
+  });
+  
+  // section paragraph fade in / out //
+  
+  const sectionParagraphs = document.querySelectorAll('.section-paragraph');
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+        entry.target.classList.remove('fade-out');
+      } else {
+        entry.target.classList.remove('fade-in');
+        entry.target.classList.add('fade-out');
+      }
+    });
+  }, {
+    threshold: 0.5 // Adjust the threshold value to control the fade-in and fade-out points
+  });
+  
+  sectionParagraphs.forEach(paragraph => {
+    observer.observe(paragraph);
+  });
+  
+    // introduction section fade in / out //
+
+    const introductionSection = document.querySelector('.introduction');
+
+    const introductionObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade-in');
+          entry.target.classList.remove('fade-out');
+        } else {
+          entry.target.classList.remove('fade-in');
+          entry.target.classList.add('fade-out');
+        }
+      });
+    }, {
+      threshold: 0.5 // Adjust the threshold value to control the fade-in and fade-out points
+    });
+    
+    introductionObserver.observe(introductionSection);
